ad9d632bcc0ff37971c7c4ef7b5543516b771370
Akshay Kumar <akshay.kumar@smallcase.com>
2022-12-04 04:37:40 +0530

    Add components for list and mint nft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
+import NftList from './components/NftList';
+import MintNft from './components/MintNft';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Nft List</h1>
+      <NftList/>
+
+      <h1>Mint Nft</h1>
+      <MintNft/>
     </div>
   );
 }
